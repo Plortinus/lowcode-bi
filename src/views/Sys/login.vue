45c1f8e58f4380207c172acf8ba9274566fa8d41
Plortinus <Plortinus@gmail.com>
2023-08-29 23:47:58 +0800

    feat: login

---
+++ src/views/Sys/login.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+const form = reactive({
+  username: '',
+  password: ''
+})
+</script>
+
+<template>
+  <div>
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.usename" />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password" show-password />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
